be43c8387e8745cb57327fb59e0293de3abdfc41
Naveed Janmohamed <naveed.janmohamed@icloud.com>
2022-01-10 10:23:39 +0000

    typo fix

--- components/Modal.js
+++ components/Modal.js
@@ -4,7 +4,7 @@ const ModalComponent = ({toggleModal}) => (
     <>
         <div className="modal">
             <h1>Welcome to the Floobiverze</h1>
-            <p className="p-pad">Floobz are a series of NFTs created using a combination of shaders for the web and Three.js. The animations you're seeing are being pulled directly from an NFT smart contract published on the Ethereum Rinkeby Test Network.</p>
+            <p className="p-pad">Floobz are a series of NFTs created using a combination of shaders for the web and Three.js. The animations you`&apos`re seeing are being pulled directly from an NFT smart contract published on the Ethereum Rinkeby Test Network.</p>
             <p className="p-pad">I wrote the contracts in Solidity and built this app using React. View the repo <a href="https://github.com/naveedjan/floobz" target="_blank" rel="noreferrer">here</a>.</p>
             <p>Made by <a href="https://www.naveedjan.com/" target="_blank" rel="noreferrer">me</a>.</p>
         </div>
